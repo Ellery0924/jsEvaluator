fbcc038d3babd0c69e8a4263b53726062490a0ae
Ellery0924 <383188743@qq.com>
2016-12-21 02:45:06 +0800

    while do实现，jseval已经完成

--- test/evalTestCode.js
+++ test/evalTestCode.js
@@ -1,118 +1,35 @@
-for (var i = 0; i < 10; i++) {
-    console.log('i', i);
-    for (var j = 0; j < 5; j++) {
-        console.log('j', j);
-        if (j >= 3) {
-            console.log('j break at', j);
-            break;
-            console.log('impossible');
+function quickSort(arr, start, end) {
+    if (start >= end) {
+        return;
+    }
+
+    var q = partition(arr, start, end);
+    quickSort(arr, start, q - 1);
+    quickSort(arr, q + 1, end);
+}
+
+function swap(arr, i, j) {
+    var tmp = arr[j];
+
+    arr[j] = arr[i];
+    arr[i] = tmp;
+}
+
+function partition(arr, start, end) {
+    var r = arr[end], j, i;
+    j = start;
+
+    for (i = start; i < end; i++) {
+        if (arr[i] < r) {
+            swap(arr, i, j);
+            j++;
         }
     }
 
-    if (i > 7) {
-        console.log('i break at', i);
-        break;
-    }
+    swap(arr, end, j);
+    return j;
 }
 
-function bubbleSort(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        for (var j = 0; j < i; j++) {
-            var tmp = array[j + 1];
-            if (array[j] > array[j + 1]) {
-                array[j + 1] = array[j];
-                array[j] = tmp;
-            }
-        }
-    }
-}
-
-function factorial(n) {
-    if (n === 1) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
-}
-
-function fibonacci(n) {
-    if (n === 1 || n === 2) {
-        return 1;
-    } else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
-}
-
-function Parent() {
-}
-
-Parent.prototype = {
-    foo: function () {
-        console.log('i am from klass!');
-    }
-};
-
-function Child() {
-}
-
-Child.prototype = new Parent();
-Child.prototype.bar = function () {
-    console.log('i am from child!')
-};
-
-function closure() {
-    var clsVar = { value: 0 };
-
-    return {
-        plus: function () {
-            clsVar.value++;
-            console.log('plus', clsVar);
-            return this;
-        },
-        minus: function () {
-            clsVar.value--;
-            console.log('minus', clsVar);
-            return this;
-        }
-    };
-}
-
-function hof(foo) {
-    var another = { value: 1000 };
-    return function (a, b) {
-        return foo(a.value, b.value) + another.value;
-    };
-}
-
-var arr = [5, 4, 3, 2, 1];
-bubbleSort(arr);
+var arr = [9, 40, 33, 22, 1];
+quickSort(arr, 0, arr.length - 1);
 console.log(arr);
-console.log(factorial(5));
-console.log(fibonacci(10));
-
-var child = new Child();
-child.foo();
-child.bar();
-console.log(child instanceof Child);
-
-var cls1 = closure();
-var cls2 = closure();
-console.log('cls1');
-cls1.minus().plus().minus();
-console.log('cls2');
-cls2.plus().plus().minus();
-
-var hoftest = hof(function (a, b) {
-    return a * b;
-});
-
-var ret = hoftest({ value: 200 }, { value: 10 });
-console.log(ret);
-
-var testOb = {
-    foo: function () {
-        return 1000;
-    }
-}
-
-console.log(typeof testOb.foo);

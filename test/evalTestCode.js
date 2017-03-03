d91c87a463514002e2ae50e161b9beb30c7c158b
Ellery0924 <383188743@qq.com>
2017-03-03 14:03:20 +0800

    test modify

--- test/evalTestCode.js
+++ test/evalTestCode.js
@@ -116,10 +116,10 @@ function insertionsort(arr) {
 }
 
 function bubblesort(arr) {
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = i + 1; j < arr.length; j++) {
-            if (arr[i] > arr[j]) {
-                swap(arr, i, j);
+    for (var i = 0; i < arr.length - 1; i++) {
+        for (var j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
             }
         }
     }
